b54a3cc848f8494dc9ff637fb7b27fac54f85115
Vallee Carey <60826335+ValleeCarey@users.noreply.github.com>
2023-01-18 21:19:51 -0600

    Update shopping.py

--- shopping.py
+++ shopping.py
@@ -41,16 +41,16 @@ def check_taxable(shopping_cart):
     return {'subtotal': taxable_subtotal + non_taxable_subtotal, 'tax_amount': tax_amount, 'grand_total': grand_total}
 
 #Testcases
-print('Subtotal: $' + str(total['subtotal']) + ' USD')
-print('Tax amount: $' + str(total['tax_amount']) +  ' USD')
-print('Grand total: $' + str(total['grand_total']) + ' USD')
+# print('Subtotal: $' + str(total['subtotal']) + ' USD')
+# print('Tax amount: $' + str(total['tax_amount']) +  ' USD')
+# print('Grand total: $' + str(total['grand_total']) + ' USD')
 
 
 #Calculate Feature 4
 def apply_coupon(shopping_cart, coupon_code):
     shopping_cart = [{'name': 'item1', 'price': 10.00}, {'name': 'item2', 'price': 15.00}, {'name': 'item3', 'price': 20.00}]
 
-    data = {"coupon1": {"valid": True, "items": ["item1, item2"], "discount": 5.00}}, {"coupon2": {"valid": True, "items": ["item3"], "discount": 10.00}},{"coupon3": {"valid": False,  "items": ["item4"], "discount": 15.00}}
+    data = [{"coupon1": {"valid": True, "items": ["item1, item2"], "discount": 5.00}}, {"coupon2": {"valid": True, "items": ["item3"], "discount": 10.00}},{"coupon3": {"valid": False,  "items": ["item4"], "discount": 15.00}}]
 
     temp_file = json.dumps(data, indent = 3)
     with open("coupon.json", "w") as file:
@@ -58,25 +58,24 @@ def apply_coupon(shopping_cart, coupon_code):
 
     with open('coupon.json', 'r') as file:
         coupons = json.load(file)
-        #print(type(coupons))
-        if coupon_code in coupons:
-            print(coupon_code)
-            coupon = coupons[coupon_code]
-            valid = coupon['valid'] 
-            if valid:
+        for item in coupons:
+            for i in item:
+        #for coupon_code in coupons:
+                coupon = item[i] #i is coupons1, 2, 3
+                valid = coupon['valid'] #access valid items and discount
+                if valid:
         #checking to see if any of the coupons applies to the items in the cart.
-                for item in shopping_cart:
-                    if item['name'] in coupon['items']:
-                        item['price'] -= coupon['discount']
-                        print(item['price'])
-                        break
+                    for item in shopping_cart:
+                        if item['name'] in coupon['items']:
+                            item['price'] -= coupon['discount']
+                            break
+                    else:
+                        print(f"{coupon_code} is not applied to any items in the cart.")
                 else:
-                    print(f"{coupon_code} is not applied to any items in the cart.")
-            else:
-                print(f"{coupon_code} is not a valid coupon")
+                    print(f"{coupon_code} is not a valid coupon")
 
-apply_coupon(shopping_cart, 'coupon1')
+coup = apply_coupon(shopping_cart, "coupon3")
 total = calculate_total(shopping_cart)
-# print('Subtotal: $' + str(total['subtotal']) + ' USD')
-# print('Tax amount: $' + str(total['tax_amount']) +  ' USD')
-# print('Grand total: $' + str(total['grand_total']) + ' USD')
+print('Subtotal: $' + str(total['subtotal']) + ' USD')
+print('Tax amount: $' + str(total['tax_amount']) +  ' USD')
+print('Grand total: $' + str(total['grand_total']) + ' USD')
